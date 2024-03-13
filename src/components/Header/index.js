48d72e4ce050480dcf3966a9689068431f38a77b
akash <akashgs101.com>
2024-03-13 07:38:06 +0000

    1

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import './index.css'
+
+const Header = () => {
+  const a = 'A'
+  return (
+    <div>
+      <div className="header-container">
+        <div className="con1">
+          <h1 className="main-heading">COVID19INDIA</h1>
+          <img
+            className="nav-img"
+            src="https://tse3.mm.bing.net/th?id=OIP.pXSNZCRCUOsC-ClOBQk5VQHaHa&pid=Api&P=0&h=180s"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
